266320d5cca480a043f1362fb56ad6bea2705178
RAFA <wicaksonorafha@gmail.com>
2022-04-26 09:34:46 +0700

    shopan app

--- app/src/main/java/com/example/shopan/LoginActivity.kt
+++ app/src/main/java/com/example/shopan/LoginActivity.kt
@@ -4,24 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val btnRegister = findViewById<Button>(R.id.btnRegisterOnLogin)
+        val firebaseApp = FirebaseApp.initializeApp(this)
+        auth = firebaseApp?.let { FirebaseAuth.getInstance(it) }
+
+        val btnRegister: Button = findViewById(R.id.btnRegisterOnLogin)
         btnRegister.setOnClickListener {
-            Intent(this@LoginActivity,RegisterActivity::class.java).also{
-                startActivity(intent)
-            Toast.makeText(this,"please make an account", Toast.LENGTH_SHORT).show()
+            Intent(this@LoginActivity, RegisterActivity::class.java).also {
+                startActivity(it)
+                Toast.makeText(this, "please make an account", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         val btnLogin = findViewById<Button>(R.id.btnLoginOnLogin)
         btnLogin.setOnClickListener {
-            Toast.makeText(this, "Login succesful", Toast.LENGTH_SHORT).show()
-
+            // do something
+            val email: EditText = findViewById(R.id.et_email_onlogin)
+            val password: EditText = findViewById(R.id.et_password_onlogin)
+            auth?.signInWithEmailAndPassword(email.text.toString(), password.text.toString())?.addOnSuccessListener {
+                Intent(this@LoginActivity, HomeActivity::class.java).also{
+                    startActivity(it)
+                }
+            }
         }
-    }}}}
+    }
+}
 
--- app/src/main/java/com/example/shopan/RegisterActivity.kt
+++ app/src/main/java/com/example/shopan/RegisterActivity.kt
@@ -4,17 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val buttonRegister = findViewById<Button>(R.id.btnRegisterOnLogin)
-        buttonRegister.setOnClickListener {
-            val intent= Intent(this@RegisterActivity, LoginActivity::class.java).also{
-                startActivity(intent)
-            Toast.makeText(this, "Button register Clicked", Toast.LENGTH_SHORT).show()
+        val firebaseApp = FirebaseApp.initializeApp(this)
+        auth = firebaseApp?.let { FirebaseAuth.getInstance(it) }
+
+        val btnlogin: Button = findViewById(R.id.btnRegisterOnRegister)
+        btnlogin.setOnClickListener {
+            Intent(this@RegisterActivity, LoginActivity::class.java).also {
+                startActivity(it)
+                Toast.makeText(this, "please enter your account", Toast.LENGTH_SHORT).show()
+            }
         }
-}}}
+
+        val btnRegister = findViewById<Button>(R.id.btnLoginOnRegister)
+        btnRegister.setOnClickListener {
+            // do something
+            val email: EditText = findViewById(R.id.et_email_onregister)
+            val password: EditText = findViewById(R.id.et_password_onregister)
+            auth?.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+        }
+    }
+}
